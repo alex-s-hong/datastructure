4a351058e514c56a56b0bd8bb95b6a2028faf885
Alex Hong <Alex Hong>
2016-06-24 02:47:23 +0900

    hashing has been completed. Building sorted list with the aid of heapsort, but it doesn't work out as I intended(descending order is what I wanted, but it displays the ascending one.)

--- test_import.py
+++ test_import.py
@@ -1,4 +1,258 @@
+WHITE = 0
+GRAY = 1
+BLACK = 2
 
+def parent(n):
+    return (n-1)/2
+
+def left(n):
+    return 2*n+1
+
+def right(n):
+    return 2*n+2
+
+def heapify(A,i,heapsize):
+    l=left(i)
+    r=right(i)
+    if l<heapsize and A[l]>A[i]:
+        largest =l
+    else:
+        largest=i
+    if r<heapsize and A[r]>A[largest]:
+        largest=r
+    if largest!=i:
+        A[i], A[largest]=A[largest],A[i]
+        heapify(A,largest,heapsize)
+
+def build_a_heap(A):
+    for i in range(len(A)//2,0,-1):
+        heapify(A,i-1,len(A))
+def heapsort(A):
+    build_a_heap(A)
+    for i in range(len(A),1,-1):
+        A[i-1],A[0]=A[0],A[i-1]
+        heapify(A,0,i-1)
+
+
+class Adj:
+    def __init__(self):
+        self.n = 0
+        self.next = None
+
+
+class Vertex:
+    def __init__(self, name):
+        self.color = WHITE
+        self.parent = -1
+        self.name = name
+        self.n = 0
+        self.first = None
+
+    def add(self, v):
+        a = Adj()
+        a.n = v.n
+        a.next = self.first
+        self.first = a
+
+    def copy(self, other):
+        self.color = other.color
+        self.parent = other.parent
+        self.name = other.name
+        self.n = other.n
+        self.first = other.first
+
+
+class DFSVertex(Vertex):
+    def __init__(self, name):
+        super().__init__(name)
+        self.d = 0
+        self.f = 0
+
+    def copy(self, other):
+        super().copy(other)
+        self.d = other.d
+        self.f = other.f
+
+
+class Queue:
+    def __init__(self):
+        self.front = 0
+        self.rear = 0
+        self.sz = 0
+        self.buf = []
+
+    def create_queue(self, sz):
+        self.sz = sz
+        self.buf = list(range(sz))  # malloc(sizeof(int)*sz)
+
+    def enqueue(self, val):
+        self.buf[self.rear] = val
+        self.rear = (self.rear + 1) % self.sz
+
+    def dequeue(self):
+        res = self.buf[self.front]
+        self.front = (self.front + 1) % self.sz
+        return res
+
+    def is_empty(self):
+        return self.front == self.rear
+
+
+def print_vertex(vertices, n):
+    print(vertices[n].name, end=' ')
+    print(vertices[n].color, end=' ')
+    print(vertices[n].parent, end=' ')
+    print(vertices[n].d, end=':')
+    p = vertices[n].first
+    while p:
+        print(vertices[p.n].name, end=' ')
+        p = p.next
+    print('')
+
+
+def g_transpose(vertices, vertices1):
+    for i in range(len(vertices1)):
+        vertices1[i].first = None
+    for v in vertices:
+        p = v.first
+        while p:
+            vertices1[p.n].add(v)
+            p = p.next
+
+
+class DepthFirstSearch:
+    def __init__(self):
+        self.time = 0;
+        self.vertices = None
+
+    def set_vertices(self, vertices):
+        self.vertices = vertices
+        for i in range(len(self.vertices)):
+            self.vertices[i].n = i
+
+    def dfs(self):
+        for u in self.vertices:
+            u.color = WHITE
+            u.parent = -1
+        self.time = 0
+        for u in self.vertices:
+            if u.color == WHITE:
+                self.dfs_visit(u)
+
+    def dfs_visit(self, u):
+        self.time = self.time + 1
+        u.d = self.time
+        u.color = GRAY
+        v = u.first
+        while v:
+            if self.vertices[v.n].color == WHITE:
+                self.vertices[v.n].parent = u.n
+                self.dfs_visit(self.vertices[v.n])
+            v = v.next;
+        u.color = BLACK
+        self.time = self.time + 1
+        u.f = self.time
+
+    def print_scc(self, u):
+        print(u.name, end=" ")
+        vset = self.vertices
+        if u.parent >= 0:
+            self.print_scc(vset[u.parent])
+
+    def scc_find(self, u):
+        u.color = GRAY
+        v = u.first
+        found = False
+        while v:
+            if self.vertices[v.n].color == WHITE:
+                found = True
+                self.vertices[v.n].parent = u.n
+                self.scc_find(self.vertices[v.n])
+            v = v.next
+        if not found:
+            print("SCC:", end=" ")
+            self.print_scc(u)
+            print("")
+        u.color = BLACK
+
+    def print_vertex(self, n):
+        print(self.vertices[n].name, end=' ')
+        print(self.vertices[n].color, end=' ')
+        print(self.vertices[n].parent, end=' ')
+        print(self.vertices[n].d, end=' ')
+        print(self.vertices[n].f, end=':')
+        p = self.vertices[n].first
+        while p:
+            print(self.vertices[p.n].name, end=' ')
+            p = p.next
+        print('')
+
+    def print_vertices(self):
+        for i in range(len(self.vertices)):
+            self.print_vertex(i)
+
+    def transpose(self):
+        vertices1 = []
+        for v in self.vertices:
+            v1 = DFSVertex(v.name)
+            v1.copy(v)
+            vertices1.append(v1)
+        g_transpose(self.vertices, vertices1)
+        self.set_vertices(vertices1)
+
+    # def left(self, n):
+    #     return 2 * n + 1
+    #
+    # def right(self, n):
+    #     return 2 * n + 2
+    #
+    # def heapify(self, A, i, heapsize):
+    #     vset = self.vertices
+    #     l = self.left(i)
+    #     r = self.right(i)
+    #     if l < heapsize and vset[A[l]].f > vset[A[i]].f:
+    #         largest = l
+    #     else:
+    #         largest = i
+    #     if r < heapsize and vset[A[r]].f > vset[A[largest]].f:
+    #         largest = r
+    #     if largest != i:
+    #         A[i], A[largest] = A[largest], A[i]
+    #         self.heapify(A, largest, heapsize)
+    #
+    # def buildheap(self, A):
+    #     for i in range(len(A) // 2 + 1, 0, -1):
+    #         self.heapify(A, i - 1, len(A))
+    #
+    # def heapsort(self, A):
+    #     self.buildheap(A)
+    #     for i in range(len(A), 1, -1):
+    #         A[i - 1], A[0] = A[0], A[i - 1]
+    #         self.heapify(A, 0, i - 1)
+
+    def sort_by_f(self):
+        vset = self.vertices
+        sorted_indices = list(range(len(vset)))
+        self.heapsort(sorted_indices)
+        return sorted_indices
+
+    def scc(self):
+        self.dfs()
+        self.print_vertices()
+        self.transpose()
+        sorted = self.sort_by_f()
+        vset = self.vertices
+        for v in vset:
+            v.color = WHITE
+            v.parent = -1
+        for n in sorted:
+            if self.vertices[n].color == WHITE:
+                self.scc_find(vset[n])
+
+
+userprofilelist = []
+userfriendship = []
+tweets = []
 class Node:
     def __init__(self):
         self.number = 0
@@ -21,59 +275,97 @@ class Friend:
         self.following = None
 
 def menu_0 ():
-    userfile = open('user_utf.txt')
+    userfile = open('user_sample.txt')
     i = 0
-    userprofilelist = []
+
     for line in userfile:
         line = line[0:-1]
         if ((i%4) ==0): #i starts with 0, which is the starting point of each user's information.
             user = User()
-        elif ((i%4)==1):
             user.number = line
-        elif ((i%4)==2):
+        elif ((i%4)==1):
             user.date = line
-        elif ((i%4)==3):
+        elif ((i%4)==2):
             user.id = line
+        elif ((i%4)==3):
             userprofilelist.append(user)
         i = i+1
-
-    friendfile = open('friend_utf.txt')
+     #   print(line)
+    friendfile = open('friend_sample.txt')
     j = 0
-    userfriendship = []
     for line in friendfile:
         line = line [0:-1]
         if ((j%3) ==0):
             user = Friend()
-        elif ((j%3) == 1):
             user.number = line
+        elif ((j%3) == 1):
+            user.following = line
         elif ((j%3) == 2):
-            user.following =line
             userfriendship.append(user)
         j = j+1
 
-    with open("word_utf.txt", "r", encoding ="utf-8") as wordfile:
+    with open("word_sample.txt", "r", encoding ="utf-8") as wordfile:
         k =0
-        tweets = []
         for line in wordfile:
             line = line [0:-1]
             if ((k%4)==0):
                 user = Word()
-            elif ((k%4)==1):
                 user.number = line
-            elif ((k%4)==2):
+            elif ((k%4)==1):
                 user.date = line
-            elif ((k%4)==3):
+            elif ((k%4)==2):
                 user.tweet = line
+            elif ((k%4)==3):
                 tweets.append(user)
             k = k+1
-
     print("Total users:", len(userprofilelist))
     print("Total friendship records", len(userfriendship))
     print("Total tweets", len(tweets))
 
 
-def menu_1() # friends statistics: hashing?
-    
+#def menu_1(): # friends statistics: hashing?
+#    hashtable = [0:len(userprofilelist)-1]
+
+
+ #   for user in userfriendship:
+ #       if (user.number != hashtable.key)
+ #           hashtable.append()
+
+
+
+
+#word hashing
+def wordhashing():
+    word_hashtable = {}
+    for tw in tweets:
+        #print(tw.tweet)
+        if(tw.tweet in word_hashtable):
+            temp = word_hashtable.get(tw.tweet)
+            temp.append(tw.number)
+            word_hashtable[tw.tweet] = temp
+            #print(tw.tweet)
+        else:
+            word_hashtable[tw.tweet] = [tw.number]
+
+    #sorting from the most frequent words mentioned
+    frequency_list = []
+    for tw_hashkey in word_hashtable:
+        value = word_hashtable.get(tw_hashkey)
+        if(len(value) > 2):
+            set = [len(value), tw_hashkey]
+            frequency_list.append(set)
+            print(len(value), tw_hashkey)
+    heapsort(frequency_list)
+
+#    frequency_list.sort()
+#    frequency_list.reverse()
+    print("----------\n")
+    print(frequency_list)
+
+  #  print(len(tweets))
+  #  print(len(word_hashtable))
+  #  print(word_hashtable)
+
 
 
 
@@ -116,3 +408,5 @@ def MainMenu():
 #main ()
 MainMenu()
 menu_0()
+#menu_1()
+wordhashing()
